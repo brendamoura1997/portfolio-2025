a062178e34f520a7f847468570f2662560d7719f
Brenda Moura <95611970+brendamoura1997@users.noreply.github.com>
2025-05-13 13:36:11 -0300

    Ajustes na seção sobre mim

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,7 +1,7 @@
 const ImageCard = ({ imageSrc, title }) => {
   return (
     <div
-      className="relative w-[55vw] sm:w-[45vw] h-[80vw] sm:h-[75vw] max-w-[210px] sm:max-w-[190px] max-h-[320px] 
+      className="relative w-[55vw] sm:w-[45vw] sm:h-[75vw] max-w-[210px] sm:max-w-[190px]
     sm:max-h-[280px] rounded-md clip-diagonal-top-right flex flex-col items-center
     "
     >
@@ -14,7 +14,7 @@ const ImageCard = ({ imageSrc, title }) => {
       />
 
       <div className="flex justify-center">
-        <h3 className="py-4 sm:py-4 T1 md:py-1 text-[var(--light-cyan)] text-xl font-bold">
+        <h3 className="pt-4 sm:py-4 T1 md:py-1 text-[var(--light-cyan)] text-xl font-bold">
           {title}
         </h3>
       </div>
--- src/sections/About/AboutTop.jsx
+++ src/sections/About/AboutTop.jsx
@@ -12,6 +12,8 @@ const AboutTop = () => {
   const [showMore, setShowMore] = useState(false);
   const [isTabletPortrait, setIsTabletPortrait] = useState(false);
   const sectionRef = useRef(null);
+  const [clipActive, setClipActive] = useState(true);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     const checkTabletPortrait = () => {
@@ -27,6 +29,15 @@ const AboutTop = () => {
     checkTabletPortrait();
     window.addEventListener("resize", checkTabletPortrait);
     return () => window.removeEventListener("resize", checkTabletPortrait);
+  }, []);
+
+  useEffect(() => {
+    setIsMounted(true);
+    const timeout = setTimeout(() => {
+      setClipActive(false);
+    }, 400);
+
+    return () => clearTimeout(timeout);
   }, []);
 
   // GSAP Scroll Animation
@@ -62,68 +73,78 @@ const AboutTop = () => {
         style={{
           boxShadow: "0 0 20px 5px #013880",
           padding: "25px 35px",
-          border: "2px solid yellow",
         }}
-        transition={{ duration: 0.6, ease: "easeInOut" }}
-        className={`md:p-4 h-auto md:h-80 bg-[#000]/20 backdrop-blur-sm transition-all duration-500  relative 
-          rounded-lg flex flex-col md:flex-row mx-auto ${
-            showMore ? "tablet-expand" : ""
-          } `}
+        transition={{ duration: 0.2, ease: "easeInOut" }}
+        className={`md:p-4 h-auto md:h-80 bg-[#000]/20 backdrop-blur-sm transition-all duration-500 relative rounded-lg flex
+          flex-col md:flex-row mx-auto ${showMore ? "tablet-expand" : ""}
+            ${clipActive ? "clip-diagonal-top-right" : ""}`}
       >
-        <div className="flex justify-center items-center py-6 md:py-0 ">
-          <ImageCard imageSrc={profileImage} title="BRENDA MOURA" />
-        </div>
+        {isMounted ? (
+          <>
+            <div className="flex justify-center items-center py-1 md:py-0">
+              <ImageCard imageSrc={profileImage} title="BRENDA MOURA" />
+            </div>
 
-        <div className="flex md:ml-5 w-full pb-20 md:pb-0">
-          <div
-            className="flex-1 flex flex-col text-sm md:text-md BODY1 text-justify leading-relaxed gap-4
+            <div className="flex md:ml-5 w-full sm:pb-20 md:pb-0 ">
+              <div
+                className="flex-1 flex flex-col text-sm md:text-md BODY1 text-justify leading-relaxed gap-4
           text-[var(--light-gray)] pb-10 justify-center"
-          >
-            <p>
-              Bacharel em Ciência da Computação com foco em Desenvolvimento Web
-              Full Stack e Design de Interfaces.
-            </p>
-            <p>
-              Minha experiência abrange tecnologias de frontend e backend, além
-              de princípios de UI/UX, permitindo-me criar aplicações web
-              completas com foco em desempenho e usabilidade.
-            </p>
-            <p>
-              Atuei como desenvolvedora freelancer, desenvolvendo interfaces
-              responsivas, APIs escaláveis e sistemas integrados.
-            </p>
-            <AnimatePresence>
-              {showMore && (
-                <motion.div
-                  initial={{ opacity: 0, y: -20, width: "100%" }}
-                  animate={{ opacity: 1, y: 0, width: "100%", height: "auto" }}
-                  exit={{ opacity: 0, y: -20 }}
-                  transition={{ duration: 0.5 }}
-                  className="w-full md:w-auto"
-                >
-                  <p>
-                    Tenho uma base sólida em engenharia de software, com prática
-                    em frameworks modernos como React, Node.js e Next.js. Minhas
-                    habilidades incluem arquitetura de sistemas, integração com
-                    bancos de dados e desenvolvimento de interfaces intuitivas.
-                    A combinação de competências técnicas e sensibilidade para
-                    design me permite entregar soluções web que aliam
-                    funcionalidade, estética e ótima experiência do usuário.
-                  </p>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
-        </div>
+              >
+                <p>
+                  Bacharel em Ciência da Computação com foco em Desenvolvimento
+                  Web Full Stack e Design de Interfaces.
+                </p>
+                <p>
+                  Minha experiência abrange tecnologias de frontend e backend,
+                  além de princípios de UI/UX, permitindo-me criar aplicações
+                  web completas com foco em desempenho e usabilidade.
+                </p>
+                <p>
+                  Atuei como desenvolvedora freelancer, desenvolvendo interfaces
+                  responsivas, APIs escaláveis e sistemas integrados.
+                </p>
+                <AnimatePresence>
+                  {showMore && (
+                    <motion.div
+                      initial={{ opacity: 0, y: -20, width: "100%" }}
+                      animate={{
+                        opacity: 1,
+                        y: 0,
+                        width: "100%",
+                        height: "auto",
+                      }}
+                      exit={{ opacity: 0, y: -20 }}
+                      transition={{ duration: 0.2, ease: "easeInOut" }}
+                      className="w-full md:w-auto flex flex-col gap-4"
+                    >
+                      <p>
+                        Tenho uma base sólida em engenharia de software, com
+                        prática em frameworks modernos como React, Node.js e
+                        Next.js. Minhas habilidades incluem arquitetura de
+                        sistemas, integração com bancos de dados e
+                        desenvolvimento de interfaces intuitivas.
+                      </p>
+                      <p>
+                        A combinação de competências técnicas e sensibilidade
+                        para design me permite entregar soluções web que aliam
+                        funcionalidade, estética e ótima experiência do usuário.
+                      </p>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
+            </div>
 
-        <div className="absolute bottom-4 right-6">
-          <NeonButton
-            width="w-full md:w-48 text-sm md:text-md"
-            paddingY="py-3"
-            text={showMore ? "Minimizar" : "Veja mais"}
-            onClick={() => setShowMore(!showMore)}
-          />
-        </div>
+            <div className="sm:absolute sm:bottom-4 sm:right-6">
+              <NeonButton
+                width="w-full text-sm md:text-md px-6 py-2 sm:w-[140px] sm:h-[45px] relative"
+                paddingY="py-3"
+                text={showMore ? "Minimizar" : "Veja mais"}
+                onClick={() => setShowMore(!showMore)}
+              />
+            </div>
+          </>
+        ) : null}
       </motion.div>
     </section>
   );
