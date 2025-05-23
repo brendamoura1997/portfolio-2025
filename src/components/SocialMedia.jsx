ec099be9f9ef9de80adbeb7fb467c6a256c29dfa
Brenda Moura <95611970+brendamoura1997@users.noreply.github.com>
2025-05-23 01:59:34 -0300

    Iniciando as mudanças

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -19,8 +19,8 @@ const socialLinks = [
   },
 ];
 
-const SocialMedia = ({ addClassName, gap }) => (
-  <div className={`flex justify-center ${gap} mt-10 flex-wrap`}>
+const SocialMedia = ({ addClassName, addClassNameDiv, showLabel }) => (
+  <div className={`flex ${addClassNameDiv}`}>
     {socialLinks.map(({ href, icon, alt }, index) => (
       <motion.a
         key={alt}
@@ -35,18 +35,31 @@ const SocialMedia = ({ addClassName, gap }) => (
         animate={{
           opacity: 1,
           x: 0,
-          filter: "drop-shadow(0px 0px 5px var(--neon-cyan))",
+          // filter: "drop-shadow(0px 0px 5px var(--neon-cyan))",
+          filter: `${
+            showLabel
+              ? "drop-shadow(0px 0px 2px var(--neon-cyan))"
+              : "drop-shadow(0px 0px 5px var(--neon-cyan))"
+          }`,
         }}
         transition={{ duration: 0.5, delay: index * 0.2 }}
-        className="rounded-full"
+        className="flex items-center gap-2 group transition duration-150 ease-in-out"
       >
         <img
           src={icon}
           alt={alt}
-          className={` ${addClassName} rounded-full border border-[var(--neon-cyan)]/30 
-           hover:brightness-[2] hover:shadow-[0_0_10px_#00ffcc] hover:border-[var(--neon-cyan)]
-          active:scale-90 transition duration-150 ease-in-out`}
+          className={`${addClassName} rounded-full border border-[var(--neon-cyan)]/30 
+            group-hover:brightness-[2] group-hover:shadow-[0_0_10px_#00ffcc] group-hover:border-[var(--neon-cyan)]
+            active:scale-90 transition duration-150 ease-in-out`}
         />
+        {showLabel && (
+          <span
+            className="text-[var(--neon-cyan)]/70 BODY1 text-sm transition duration-150 ease-in-out
+           group-hover:text-[var(--light-cyan-title)] group-hover:drop-shadow-[0_0_6px_#00ffcc]"
+          >
+            {alt}
+          </span>
+        )}
       </motion.a>
     ))}
   </div>
--- src/sections/Layout/Footer.jsx
+++ src/sections/Layout/Footer.jsx
@@ -11,31 +11,60 @@ const Footer = () => {
   return (
     <footer
       ref={footerRef}
-      className="text-white w-full h-auto pt-8 md:p-5 pb-10 px-6 bg-[#000000]/50 backdrop-blur-sm  overflow-hidden 
+      className="text-white w-full h-auto pt-8 md:pt-13 pb-20 px-6 bg-[#000000]/50 backdrop-blur-sm overflow-hidden 
       flex flex-col justify-center text-center"
     >
       {/* Footer Content */}
-      <div className="">
-        {/* Left Section: Social Links */}
-        <div className="flex flex-col justify-center text-center">
+      <div className="content grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-20 max-w-5xl mx-auto">
+        {/* COLUNA 1 */}
+        <div className="flex flex-col items-start text-start">
+          <p className="T2 text-[var(--neon-cyan)]/80 font-semibold text-md mb-2">
+            Redes Sociais
+          </p>
           <SocialMedia
-            addClassName={"w-10 h-10 sm:w-11 sm:h-11 brightness-[0.8]"}
-            gap={"gap-3 lg:gap-10"}
+            addClassNameDiv={"gap-3 lg:gap-2 flex-col"}
+            addClassName={"w-10 h-10 sm:w-6 sm:h-6 brightness-[0.8]"}
+            showLabel={true}
           />
+        </div>
 
-          {/* Footer Text */}
-
-          <p className="text-gray-300 text-sm mt-5">
-            © 2025 BRENDA MOURA | All Rights Reserved.
+        {/* COLUNA 2 */}
+        <div className="flex flex-col items-center md:items-start text-center md:text-start gap-2">
+          <p className="T2 text-[var(--neon-cyan)]/80 font-semibold text-md">
+            Contato
+          </p>
+          <p className="text-[var(--neon-cyan)]/75 BODY1 text-sm">
+            • brendamoura857@gmail.com
+          </p>
+          <p className="text-[var(--neon-cyan)]/75 BODY1 text-sm">
+            • 11 94300-0131
+          </p>
+          <p className="text-[var(--neon-cyan)]/75 BODY1 text-sm">
+            • Barueri, São Paulo
           </p>
         </div>
-      </div>
 
-      {/* Animated Neon Line */}
-      <span
-        className="absolute bottom-1 left-1/2 w-full h-[2px] -translate-x-1/2 bg-gradient-to-r from-transparent 
-    via-[#00ffff] to-transparent opacity-100 hover:via-[#b8ffff]"
-      />
+        {/* COLUNA 3 */}
+        <div className="flex flex-col items-center md:items-start text-center md:text-start gap-2">
+          <p className="T2 text-[var(--neon-cyan)]/80 font-semibold text-md">
+            Disponibilidade
+          </p>
+          <p className="text-[var(--neon-cyan)]/75 BODY1 text-sm">
+            • Disponível para CLT ou PJ
+          </p>
+          <p className="text-[var(--neon-cyan)]/75 BODY1 text-sm">
+            • Apenas remoto
+          </p>
+          <p className="text-[var(--neon-cyan)]/75 BODY1 text-sm">
+            • Horário e valores à combinar
+          </p>
+        </div>
+        {/* Neon Line */}
+        <span
+          className="absolute bottom-0 left-1/2 w-full h-[1px] -translate-x-1/2 bg-gradient-to-r from-transparent 
+    via-[#00ffff] to-transparent opacity-50 hover:via-[#b8ffff]"
+        />
+      </div>
     </footer>
   );
 };
